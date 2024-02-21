3ea92dcf1d03ffe4eeafdacff65ceec89330d9c2
cristy Jane <cristyjanellanita13@gmail.com>
2024-02-21 09:43:50 +0800

    registration

--- mydb.py
+++ mydb.py
@@ -1,7 +1,7 @@
 import mysql.connector
 
 dataBase = mysql.connector.connect(
-    host ="localhost",
+    host ='127.0.0.1',
     user ="root",
     passwd =""
 
--- website/views.py
+++ website/views.py
@@ -1,3 +1,32 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login, logout
+from django.contrib import messages
 
-# Create your views here.
+
+#request means anytime you go a website, u r accessing and requesting dtaa ng
+#data from the back end 
+def home(request):
+    if request.method == 'POST':
+        username = request.POST ['username']
+        password = request.POST ['password']
+        #authenticate
+        user =authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            messages.success(request, "You Have Been Logged In!")
+            return redirect ('home')
+        else:
+            messages.success(request, "There was an error logging in. Please try again!")
+            return redirect ('home')
+    else:
+        return render(request, 'home.html', {})
+
+
+
+def logout_user(request):
+    logout(request)
+    messages.success(request, "You have been logout...")
+    return redirect("home")
+
+def register_user(request):
+    return render(request, 'register.html', {})
